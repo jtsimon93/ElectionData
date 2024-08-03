d13ddc8117278d199b845753f33b76e7deb497cd
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 13:47:26 -0400

    Setup configuration / db context

--- ElectionData.API/Program.cs
+++ ElectionData.API/Program.cs
@@ -1,13 +1,30 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ElectionData.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureAppConfiguration((context, config) =>
     {
+        config.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
+
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        // Configure the DbContext
+        services.AddDbContext<ElectionDataDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
     })
     .Build();
 
