2ef059c7409f3b3648b6fb80b5a0f8f03a21a01b
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 14:17:50 -0400

    Create PollDto.cs

---
+++ ElectionData.Data/Dtos/PollDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionData.Data.Dtos
+{
+    public class PollDto
+    {
+        public int Id { get; set; }
+        public string PollLink { get; set; } = string.Empty;
+        public string Pollster { get; set; } = string.Empty;
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public string? SampleSize { get; set; }
+        public string? SampleType { get; set; }
+        public decimal? MoE { get; set; }
+        public decimal? Trump { get; set; }
+        public decimal? Harris { get; set; }
+        public string? Spread { get; set; }
+    }
+}
