fd30adc4837e39f8fb8ab87413f602b8ec4e9db1
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 15:18:20 -0400

    Create DesignTimeDbContextFactory.cs

---
+++ ElectionData.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionData.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ElectionDataDbContext>
+    {
+        public ElectionDataDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.appSettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ElectionDataDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseNpgsql(connectionString);
+
+            return new ElectionDataDbContext(builder.Options);
+        }
+    }
+}
