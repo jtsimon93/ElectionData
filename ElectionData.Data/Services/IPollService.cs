aa10758ab7148bcac5edfd379535f135c8083823
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 14:18:09 -0400

    Add poll service

---
+++ ElectionData.Data/Services/IPollService.cs
@@ -0,0 +1,16 @@
+﻿using ElectionData.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionData.Data.Services
+{
+    public interface IPollService
+    {
+        Task AddPollAsync(CleanPoll poll);
+        Task<bool> CheckIfPollExists(string pollLink);
+        Task ProcessIncomingPolls(IEnumerable<CleanPoll> polls);
+    }
+}
---
+++ ElectionData.Data/Services/PollService.cs
@@ -0,0 +1,41 @@
+﻿using ElectionData.Data.Entities;
+using ElectionData.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionData.Data.Services
+{
+    public class PollService : IPollService
+    {
+        private readonly IPollRepository _pollRepository;
+
+        public PollService(IPollRepository pollRepository)
+        {
+            _pollRepository = pollRepository;
+        }
+
+        public async Task AddPollAsync(CleanPoll poll)
+        {
+            await _pollRepository.AddAsync(poll);
+        }
+
+        public async Task<bool> CheckIfPollExists(string pollLink)
+        {
+            return await _pollRepository.CheckIfPollExists(pollLink);
+        }
+
+        public async Task ProcessIncomingPolls(IEnumerable<CleanPoll> polls)
+        {
+            foreach (var poll in polls)
+            {
+                if (!await CheckIfPollExists(poll.PollLink))
+                {
+                    await AddPollAsync(poll);
+                }
+            }
+        }
+    }
+}
