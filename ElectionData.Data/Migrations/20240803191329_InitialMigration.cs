6b8a28b92130140c4a6d1a1ecdfc4f625ece9b57
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 15:18:42 -0400

    Initial migrations

---
+++ ElectionData.Data/Migrations/20240803191329_InitialMigration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ElectionData.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PollLink = table.Column<string>(type: "text", nullable: false),
+                    Pollster = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    SampleSize = table.Column<int>(type: "integer", nullable: true),
+                    SampleType = table.Column<string>(type: "text", nullable: true),
+                    MoE = table.Column<decimal>(type: "numeric", nullable: true),
+                    Trump = table.Column<decimal>(type: "numeric", nullable: false),
+                    Harris = table.Column<decimal>(type: "numeric", nullable: false),
+                    Spread = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Polls_PollLink",
+                table: "Polls",
+                column: "PollLink",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
