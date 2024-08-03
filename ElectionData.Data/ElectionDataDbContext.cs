d6a5e0928332afb9c9b13964fa67ea6fcf35ef9e
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 13:27:15 -0400

    Create ElectionDataDbContext.cs

---
+++ ElectionData.Data/ElectionDataDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectionData.Data.Entities;
+
+namespace ElectionData.Data
+{
+    public class ElectionDataDbContext : DbContext
+    {
+        public ElectionDataDbContext(DbContextOptions<ElectionDataDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CleanPoll> CleanPolls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
