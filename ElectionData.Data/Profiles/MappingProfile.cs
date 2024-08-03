0bfd625024e084e73f7bd587a7973953a6e65047
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 16:54:15 -0400

    Create MappingProfile.cs

---
+++ ElectionData.Data/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionData.Data.Profiles
+{
+    using AutoMapper;
+    using ElectionData.Data.Dtos;
+    using ElectionData.Data.Entities;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CleanPoll, PollDto>();
+        }
+    }
+
+}
