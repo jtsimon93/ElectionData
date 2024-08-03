193c60b939b3631da946bc33aa7afa989616f443
Justin Simon <jtsimon@jtsimon.dev>
2024-08-03 13:20:54 -0400

    Add entities

---
+++ ElectionData.Data/Entities/CleanPoll.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectionData.Data.Entities
+{
+    public class CleanPoll
+    {
+        [Key]
+        public string PollLink { get; set; }
+
+        [Required]
+        public string Pollster { get; set; }
+
+        public DateOnly? StartDate { get; set; }
+
+        public DateOnly? EndDate { get; set; }
+
+        public string? Sample { get; set; }
+
+        public decimal? MoE { get; set; }
+
+        public decimal? Trump { get; set; }
+
+        public decimal? Harris { get; set; }
+
+        public string? Spread { get; set; }
+    }
+}
---
+++ ElectionData.Data/Entities/DirtyPoll.cs
@@ -0,0 +1,14 @@
+﻿namespace ElectionData.Data.Entities
+{
+    public class DirtyPoll
+    {
+        public string Pollster { get; set; }
+        public string Date { get; set; }
+        public string? Sample { get; set; }
+        public string? MoE { get; set; }
+        public string Trump { get; set; }
+        public string Harris { get; set; }
+        public string? Spread { get; set; }
+        public string? PollLink { get; set; }
+    }
+}
