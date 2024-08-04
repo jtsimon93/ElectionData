d36dea231807759073d96e908bcaa633e7b72b08
Justin Simon <jtsimon@jtsimon.dev>
2024-08-04 18:07:55 -0400

    Overall page updates

--- ElectionData.Web/src/app/page.tsx
+++ ElectionData.Web/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="w-3/4 mx-auto flex flex-col gap-4 justify-center items-center">
         <div className="text-3xl">2024 Election Dashboard</div>
         <div className="text-sm">
-          Created by Justin Simon. Check out{" "}
+          Check out{" "}
           <a
             href="https://github.com/jtsimon93/ElectionData"
             target="_blank"
@@ -19,9 +19,9 @@ export default function Home() {
             referrerPolicy="no-referrer"
             className="underline"
           >
-            the project
+            the project on GitHub
           </a>
-          ,{" "}
+          .{" "}
           <a
             href="https://www.linkedin.com/in/justintsimon/"
             target="_blank"
@@ -29,13 +29,21 @@ export default function Home() {
             referrerPolicy="no-referrer"
             className="underline"
           >
-            hire me
+            Connect with me on LinkedIn
           </a>
           .
         </div>
       </div>
 
-      <div className="w-3/4 mx-auto flex flex-col md:flex-row gap-4 md:gap-10 justify-center items-center mt-5">
+      <div className="w-3/4 mx-auto flex flex-col gap-4 justify-center items-center mt-4">
+        <div className="text-lg">
+          Welcome to the 2024 Election Dashboard. This dashboard provides an
+          overview of the latest polling data for the upcoming election. You can
+          explore the latest polling data in various aggregations and formats.
+        </div>
+      </div>
+
+      <div className="w-3/4 mx-auto flex flex-col md:flex-row gap-4 md:gap-10 items-center mt-5 justify-around">
         <div className="flex-1">
           <Suspense fallback={<Loading />}>
             <LatestPollInfoBox />
@@ -56,8 +64,15 @@ export default function Home() {
 
       <div className="w-full mx-auto mt-5 md:w-3/4">
         <Suspense fallback={<Loading />}>
-          <AggregatedPollDataByPollsterChart /> 
-        </Suspense> 
+          <AggregatedPollDataByPollsterChart />
+        </Suspense>
+      </div>
+
+      <div className="w-3/4 mx-auto mt-10 text-xs text-center text-gray-500">
+        Disclaimer: The poll data presented here is collected from various
+        sources, including Real Clear Polling, through web scraping. While I
+        strive for accuracy, please be aware that this data may not be entirely
+        precise and should be interpreted accordingly.
       </div>
     </main>
   );
